82f39b5495aabbafc57d55821f696a6122cedb22
nurselaine <Elaine.t.huynh@gmail.com>
2022-11-18 13:46:01 -0800

    created array of job title, companies, locations, and date posted properties

--- src/scraping/jobScraper.js
+++ src/scraping/jobScraper.js
@@ -5,7 +5,11 @@ const url = 'https://web3.career/?page=1';
 async function getJobs() {
   // Launch browser instance
   console.log('Opening browser...');
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--disable-setuid-sandbox"],
+    'ignoreHTTPSErrors': true
+});
   // Open new page or "tab" in browser
   console.log('Opening new page...');
   const page = await browser.newPage();
@@ -14,29 +18,52 @@ async function getJobs() {
   await page.goto(url);
   // Get all job titles on page (returns array of element inner text)
   console.log('Scraping...');
-  const jobs = await page.$$eval('h2', e => e.map(el => el.innerHTML));
+  const table_row = await page.$$eval('.table_row', el => {
+    console.log(el);
+
+    // grab job title
+    let jobs = el.map(time => time.children[0].innerText);
+
+    // grab company time
+    let companies = el.map(h3 => h3.children[1].children[0].innerText);
+
+    // grab job location
+    let location = el.map(a => a.children[3].children[0].innerText)
+
+    // grab time stamps for job postings
+    let latest_post = el.map(time => time.children[2].querySelector('span').innerText);
+    return { jobs, companies, location, latest_post}
+});
+// console.log(table_row);
+//   const jobs = await page.$$eval('h2', e => {
+//     console.log(e);
+//     return e.map(el => el.innerHTML)
+//   });
   // first job posting fully displayed has the same element, so you must pop from the array to avoid this duplicate
-  jobs.pop();
+//   jobs.pop();
   // // confirm length of jobs array returned from page.$$eval
   // let listLength = jobs.length;
   // console.log('Jobs length: ', listLength);
   // console.log('Jobs:', jobs);
   // // get all company names on page (returns array of element inner text)
-  const companies = await page.$$eval('h3', e => e.map(el => el.innerHTML));
+//   const companies = await page.$$eval('h3', e => e.map(el => el.innerHTML));
   // // confirm length of companies array returned from page.$$eval
   // let companyLength = companies.length;
   // console.log('Companies length: ', companyLength);
   // console.log('Companies:', companies);
   // zip the jobs and companies together into an array of objects
-  let jobCoPairs = jobs.map((job, i) => {
+  let jobCoPairs = table_row.jobs.map((job, i) => {
     return {
       job: job,
-      company: companies[i],
+      company: table_row.companies[i],
+      location: table_row.location[i],
+      post_date: table_row.latest_post[i]
     };
   });
+
   console.log('Job/Company Pairs: ', jobCoPairs);
   // close browser instance
-  await browser.close();
+//   await browser.close();
 }
 
 getJobs();
