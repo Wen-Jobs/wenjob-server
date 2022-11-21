f89083a7ccf42715e37d2101e70529506aed9caf
nurselaine <Elaine.t.huynh@gmail.com>
2022-11-21 09:10:32 -0800

    scrape data to json file using fs library

--- src/scraping/jobScraper.js
+++ src/scraping/jobScraper.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 
 async function getJobs() {
@@ -24,7 +25,9 @@ async function getJobs() {
     let srcArr = grabTotalJobListings.split(' ');
     let totalPages = Math.ceil(+srcArr[1].replace(',', ' ') / 35);
 
-    for (let i = 1; i <= 5; i++) { // repeat in batches
+    // #job > div > div > div.col-12.col-md-8.pe-0.pe-md-5 > div > div.text-dark-grey-text.p-2.p-md-0 > div:nth-child(3)
+
+    for (let i = 1; i <= 1; i++) { // repeat in batches
         let url = `https://web3.career/?page=${i}`;
         console.log('Hitting URL...', `Page ${i}`)
         await page.goto(url);
@@ -32,11 +35,24 @@ async function getJobs() {
         console.log('Scraping...');
         // click on each job listing & grab job details
         let details = [];
-        for (let i = 1; i <= 1; i += 3) {
+        for (let i = 1; i <= 50; i += 3) {
             await page.click(`body > main > div > div > div > div.row.row-cols-2 > div:nth-child(1) > table > tbody > tr:nth-child(${i})`);
             let detail = await page.$$eval('#job > div > div', (el, i) => {
+
+                // convert string to array
+                let desc = el[0].children[2].innerText.split(' ');
+                // console.log(desc);
+
+                // find index of
+                // console.log(desc[200]);
+                // desc.slice(0, 200);
+                // console.log(desc.slice(0, 200).join(' '));
+                // let indexToSlice = desc.indexOf('RMjYwMTo2MDA6OTY4MTo1ZDIwOjljNDoyYmIxOmQ3NzE6NWIyNwM');
+                // console.log(indexToSlice);
                 return el[0].children[2].innerText;
             });
+
+            // console.log(detail);
 
             // attempted to find original job listing URL
             // let original_Listing = page.$$eval('#job > div > div > header > div.mt-2 > div > div > div.text-start.my-1.d-md-flex.justify-content-start.gap-3.d-none > a.my-btn.my-btn-primary-maximum', (el, i) => {
@@ -67,11 +83,18 @@ async function getJobs() {
             return { jobs, companies, location, latest_post, job_URL }
         });
 
+        const throwErr = (err) => {
+            if(err) throw err;
+            console.log('saved!');
+        }
+
         // Get all job information and return array of objects
         let jobCoPairs = table_row.jobs.map((job, i) => {
             let linkArr = table_row.job_URL[i].split('/');
             let key = linkArr[linkArr.length - 1];
-            return {
+
+
+            const job_listing = {
                 job: job,
                 company: table_row.companies[i],
                 location: table_row.location[i],
@@ -80,8 +103,10 @@ async function getJobs() {
                 key,
                 details: details[i],
             };
+            fs.appendFile('wenjobs_test.json', JSON.stringify(job_listing), throwErr) //method appends specified content to a file. If the file does not exist, the file will be created
+            return job_listing;
         });
-        console.log('Job/Company Pairs: ', jobCoPairs);
+        // console.log('Job/Company Pairs: ', jobCoPairs);
     }
 
 }
