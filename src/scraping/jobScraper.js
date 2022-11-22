ddcb2b821729108de63e24ae00076f8851af2e27
nurselaine <Elaine.t.huynh@gmail.com>
2022-11-21 16:23:03 -0800

    added pages 1-20 job listings

--- src/scraping/jobScraper.js
+++ src/scraping/jobScraper.js
@@ -26,7 +26,7 @@ async function getJobs() {
 
 
   // iterate over each page (swap "i <= 1" with "i <= numPages" for full scrape)
-  for (let i = 1; i <= 1; i++) {
+  for (let i = 1; i <= 10; i++) {
     const url = `https://web3.career/?page=${i}`;
     console.log('Hitting URL...', `Page ${i}`);
     await page.goto(url);
@@ -109,7 +109,7 @@ async function getJobs() {
     //method appends specified content to a file. If the file does not exist, the file will be created
     fs.appendFile('wenjobs_1_10.json', JSON.stringify(all_Jobs), throwErr);
 
-    console.log('Job/Company Pairs: ', jobCoPairs);
+    // console.log('Job/Company Pairs: ', jobCoPairs);
   }
 
   // close browser instance
